ad205b0f8b11f9d277ff06cfac0cbd0a471c19f3
gentel <149413450@qq.com>
2025-03-27 18:06:55 +0800

    2025.3.27

--- Python file/learning/1.py
+++ Python file/learning/1.py
@@ -1,32 +1,26 @@
-class person:#没有加（）默认是object的父类
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def show(self):
-        print(f"大家好我叫：{self.name},我今年是{self.age}岁")
+class person():
+    def eat(self):
+        print('人，吃五谷杂粮')
+class cat():
+    def eat(self):
+        print('猫，吃猫粮')
+class dog:
+    def eat(self):
+        print('狗，吃骨头')
+#这个三个都是一个共同点有一个同名的方法
 
-#接下来建立一个子类student去继承父类person
-class student(person):#继承了父类拥有了当中公有的内容和受保护的东西
-    def __init__(self,name,age,id):#name,age父类有可以继承，子类中新建了id
-        super().__init__(name,age)#调用父类的初始化方法
-        self.id=id
-    def show(self):
-        super().show()
-        print(f"我来自北京大学，我的学号是{self.id}")
-class doctor(person):#新建一个Doctor类
-    def __init__(self,name,age,departmen):#建了一个部门的属性
-        super().__init__(name,age)
-        self.departmen=departmen
-    def show(self):
-        # super().show()
-        #上面的父类的方法，不合适，于是下面重写了父类的方法
-        print(f"各位好我叫{self.name},我现在{self.age},我的工作是{self.departmen}")
+#编写函数
+def fun(obj):#obj形参
+    obj.eat()#通过obj调用eat方法
 
- #创建子类对象
-stu=student('tom',20,'1001')
-stu.show()
-doc=doctor('gentel',30,'surgery')
-#调用父类公有的方法
-doc.show()
+#创建三个类的对象
+per=person()
+cat=cat()
+dog=dog()
 
-    
+#调用fun函数
+fun(per)#这就是python当中的多态，不关心你的对象数据类型，只关心你这个对象是否具有同名的方法
+fun(cat)
+fun(dog)
+
+
