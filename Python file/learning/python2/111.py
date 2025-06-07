7c8bec2fcca796a3329e1608ea4263ae04bf085f
gentel <149413450@qq.com>
2025-06-07 11:38:31 +0800

    2025-6-7

--- Python file/learning/python2/111.py
+++ Python file/learning/python2/111.py
@@ -1,16 +1,5 @@
-class A:
-    def __init__(self):
-        print("A的构造方法")
-    def say(self):
-        print("A: ", self)
-        print("say AAA")
-class B(A):
-    def __init__(self):
-        super(B, self).__init__()  # 调用父类的构造方法
-        print("B的构造方法")
-    def say(self):
-        # A.say(self)   调用父类的say方法
-        super().say()  # 通过super()调用父类的方法
-        print("say BBB")
-b = B()
-b.say()
+with open("b.txt","r",encoding="utf-8") as f:
+    lines = f.readlines()
+    lines2 = [line.rstrip()+" #"+str(index)+"\n" for index,line in zip(range(1,len(lines)+1),lines)]
+with open("b.txt","w",encoding="utf-8") as f:
+    f.writelines(lines2)
